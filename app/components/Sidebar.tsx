06089a58948a5c89f918df357c2e1b4ab49796de
mittals21 <mittalsuthar112@gmail.com>
2024-09-01 19:26:19 +0530

    Review complete

--- app/components/Sidebar.tsx
+++ app/components/Sidebar.tsx
@@ -7,18 +7,18 @@ import { GoFile } from "react-icons/go"
 const Sidebar = () => {
   const [active, setActive] = useState<string>("skilltest")
   return (
-    <div className=" hidden lg:flex flex-col justify-center w-[230px] py-12 border-r-blue-600 gap-5">
+    <div className="flex lg:flex-col justify-center lg:w-[230px] pt-4 lg:py-12 border-r-blue-600 gap-5">
       <div
         className={`flex items-center gap-3 p-5 cursor-pointer ${
           active === "dashboard"
-            ? "bg-blue-300/20 rounded-r-full text-blue-500"
+            ? "bg-blue-300/20 lg:rounded-l-none rounded-full  text-blue-500"
             : ""
         }`}
         onClick={() => setActive("dashboard")}
       >
         <CgLoadbarSound className="text-2xl" />
         <p
-          className={`text-md font-semibold ${
+          className={`text-md font-semibold hidden md:block ${
             active === "dashboard" ? "text-blue-500" : "text-gray-800/70"
           }`}
         >
@@ -28,14 +28,14 @@ const Sidebar = () => {
       <div
         className={`flex items-center gap-4 p-5 cursor-pointer ${
           active === "skilltest"
-            ? "bg-blue-300/20 rounded-r-full text-blue-500"
+            ? "bg-blue-300/20 lg:rounded-l-none rounded-full text-blue-500"
             : ""
         }`}
         onClick={() => setActive("skilltest")}
       >
         <SlBadge className="text-xl" />
         <p
-          className={`text-md font-semibold ${
+          className={`text-md font-semibold hidden md:block ${
             active === "skilltest" ? "text-blue-500" : "text-gray-800/70"
           }`}
         >
@@ -45,14 +45,14 @@ const Sidebar = () => {
       <div
         className={`flex items-center gap-3 p-5 cursor-pointer ${
           active === "internship"
-            ? "bg-blue-300/20 rounded-r-full text-blue-500"
+            ? "bg-blue-300/20 lg:rounded-l-none rounded-full text-blue-500"
             : ""
         }`}
         onClick={() => setActive("internship")}
       >
         <GoFile className="text-2xl" />
         <p
-          className={`text-md font-semibold ${
+          className={`text-md font-semibold hidden md:block ${
             active === "internship" ? "text-blue-500" : "text-gray-800/70"
           }`}
         >
